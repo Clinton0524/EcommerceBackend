41f3d04d4ece93d7fa3df8621f683cb48076c76c
unknown <Admin@1111_IT01_AUD1.lulutechpark.in>
2026-09-20 20:37:49 +0530

    change

--- server.js
+++ server.js
@@ -47,7 +47,9 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://192.168.8.102:3001",
+  "http://192.168.8.102:3000",
   "https://glistening-platypus-4c4f46.netlify.app",
+   "http://192.168.137.1:3000"
 ].filter(Boolean);
 
 app.use(
